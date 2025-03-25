374f8f71ba6c1b8014e726a3367b7c2eaa476b2c
Nacho Monzón <ignaciomonzona@gmail.com>
2025-03-25 20:20:38 +0100

    Iteration 1 complete

--- js/game.js
+++ js/game.js
@@ -1,3 +1,35 @@
 class Game {
     // code to be added
+    constructor() {
+        this.startScreen = document.getElementById("game-intro");
+        this.gameScreen = document.getElementById("game-screen");
+        this.gameEndScreen = document.getElementById("game-end");
+
+        this.player = null;
+
+        this.height = 600;
+        this.width = 500;
+
+        this.obstacles = [];
+
+        this.score = 0;
+        this.lives = 3;
+
+        this.gameIsOver = false;
+
+        this.gameIntervalId = null;
+        this.gameLoopFrecuency = 1000 / 60;
+    }
+
+    start() {
+        
+    }
+
+    gameLoop() {
+       
+    }
+
+    update() {
+        
+    }
 }
